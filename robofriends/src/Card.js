a1e29d599ed1f9b5f88a130382533924fd05957f
Marko1310 <marko.cabo13@gmail.com>
2022-11-26 19:31:58 +0100

    my version

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = ({ name, email, id }) => {
+const Card = function (props) {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/${id}test?200x200`} />
+      <img alt="robot" src={`https://robohash.org/${props.id}?200x200`} />
       <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
   );
--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -1,2 +1,23 @@
 import React from "react";
 import Card from "./Card";
+import { robots } from "./robots";
+
+const Cardlist = function () {
+  return (
+    <div>
+      {robots.map((el) => {
+        return (
+          <Card
+            key={el.id}
+            id={el.id}
+            name={el.name}
+            username={el.username}
+            email={el.email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cardlist;
